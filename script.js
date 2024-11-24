cf3a8c57d6d24ed49f89c4bc7fa3be4a54f43fa6
Abualtaher <wisamaltaher@gmail.com>
2024-11-24 21:08:47 +0100

    innerHTML

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const inputBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
-const what = document.querySelector("#what");
+const ulEl = document.querySelector("#ul-el");
 
 let myLeads = [];
 
 inputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value);
   for (let i = 0; i < myLeads.length; i++) {
-    what.textContent = myLeads[i];
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li> ";
   }
 });
