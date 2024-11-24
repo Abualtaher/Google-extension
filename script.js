f78227d1a3187cafb67e44e5cc3612c423448a15
Abualtaher <wisamaltaher@gmail.com>
2024-11-24 20:55:56 +0100

    forloop

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const inputBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
+const what = document.querySelector("#what");
 
 let myLeads = [];
 
 inputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+  for (let i = 0; i < myLeads.length; i++) {
+    what.textContent = myLeads[i];
+  }
 });
