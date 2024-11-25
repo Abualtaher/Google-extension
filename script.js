1660f509821b72ee501337dade2253d459138e17
Abualtaher <wisamaltaher@gmail.com>
2024-11-25 17:18:51 +0100

    fix

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#ul-el");
 
 let myLeads = [];
+let myList = "";
 
 inputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value);
+
   for (let i = 1; i < myLeads.length; i++) {
-    /* ulEl.innerHTML += "<li>" + myLeads[i] + "</li> "; */
-    const li = document.createElement("li");
+    myList = "<li>" + myLeads[i] + "</li> ";
+    ulEl.innerHTML += myList;
+
+    /*  const li = document.createElement("li");
     li.textContent = myLeads[i];
-    ulEl.appendChild(li);
+    ulEl.appendChild(li); */
   }
 });
