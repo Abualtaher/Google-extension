016dab2972a96723959cfab3f6a4d1a7b4b8e5ca
Abualtaher <wisamaltaher@gmail.com>
2024-11-25 21:37:42 +0100

    templetString

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+let myLeads = [];
 const inputBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#ul-el");
 
-let myLeads = [];
-
 inputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value);
+  inputEl.value = "";
   renderLeads();
 });
 
 function renderLeads() {
   let myList = "";
-  for (let i = 1; i < myLeads.length; i++) {
-    myList = "<li>" + myLeads[i] + "</li> ";
+  for (let i = 0; i < myLeads.length; i++) {
+    myList = `<li>
+      <a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
+      </li > `;
   }
   ulEl.innerHTML += myList;
 }
