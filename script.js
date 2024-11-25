d96f696dff242ea4e924354074f11243a948589a
Abualtaher <wisamaltaher@gmail.com>
2024-11-25 21:07:58 +0100

    onfoucse this.value

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#ul-el");
 
 let myLeads = [];
-let myList = "";
 
 inputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value);
+  renderLeads();
+});
 
+function renderLeads() {
+  let myList = "";
   for (let i = 1; i < myLeads.length; i++) {
     myList = "<li>" + myLeads[i] + "</li> ";
-    ulEl.innerHTML += myList;
-
-    /*  const li = document.createElement("li");
-    li.textContent = myLeads[i];
-    ulEl.appendChild(li); */
   }
-});
+  ulEl.innerHTML += myList;
+}
