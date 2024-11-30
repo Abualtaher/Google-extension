f7ac7c1466e0d83fb82ed89f90b4bcc3bac2dfc4
Abualtaher <wisamaltaher@gmail.com>
2024-12-01 00:38:08 +0100

    Delete btn

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let myLeads = [];
 const inputBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#ul-el");
+const deleteBtn = document.querySelector("#delete-btn");
 
 const itemsLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
@@ -14,6 +15,12 @@ inputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value);
   inputEl.value = "";
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  renderLeads();
+});
+
+deleteBtn.addEventListener("dblclick", () => {
+  localStorage.clear();
+  myLeads = [];
   renderLeads();
 });
 
