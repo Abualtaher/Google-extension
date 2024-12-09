935ed857dd2e5a2048a73aec518c77b2eb572ea0
Abualtaher <wisamaltaher@gmail.com>
2024-12-09 22:00:30 +0100

    Save tab button

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ if (itemsLocalStorage) {
 const tabs = [{ url: "https://www.linkedin.com/in/per-harald-borgen/" }];
 
 saveTab.addEventListener("click", () => {
-  console.log(tabs[0].url);
+  myLeads.push(tabs[0].url);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
 });
 
 function render(leads) {
